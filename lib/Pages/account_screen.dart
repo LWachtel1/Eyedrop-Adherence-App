1a10d243bfa4101b070d6e021fd6209b6a909945
LWachtel1 <lwachtelproper@gmail.com>
2025-01-24 16:08:42 +0000

    Fix sign-out navigation bug

--- lib/Pages/account_screen.dart
+++ lib/Pages/account_screen.dart
@@ -10,7 +10,7 @@ class AccountScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BaseLayout(child:  ProfileScreen(
       actions: [SignedOutAction((context) {
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const AuthGate())); //buggy as it only goes right back to previous screen instead of going back to Auth Gate right away
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const AuthGate())); 
       })
       ]));
     
